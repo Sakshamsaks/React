5281a767788a204891116231ee40b88f4509f4ca
Sakshamsaks <saksham.agrawal.588@gmail.com>
2024-06-04 18:43:13 +0530

    Learning about JSX and Function Component

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent= React.createElement("div",{id:"parent"},
-    [   
-        React.createElement("div",{id:"child"},
-        [
-            React.createElement("h1",{},"This is React"),
-            React.createElement("h2",{},"jfaskfnanf")
-        ]),
-        React.createElement("div",{id:"child2"},
-        [
-            React.createElement("h1",{},"I am h1 tag"),
-            React.createElement("h2",{},"I am h2 tag")
-        ])
-    ]
+//jsx
+const Title= () => (
+    <h1 className="heading">
+        React using JSX
+    </h1>
 );
 
-// Simple hello world program using react..
-//const heading=React.createElement("h1", {id:"heading"}, "Hello world form React!");
+const title= (
+    <h1 className="heading">
+        React using JSX
+    </h1>
+);
 
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const HeadingComponent= () => (
+    <div id="container">
+        {Title()}
+        <Title />
+        <Title></Title>
+        <h1>Namaste React Functional Component</h1>
+    </div>
+);
 
-root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
